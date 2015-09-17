362e001bb199f7b2f051d4c945eac491bf6cb1a9
Cristiany Assunção <cristianyassuncao@gmail.com>
2015-09-17 09:56:47 -0300

    Apagando cÂódigo comentado

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -2,8 +2,6 @@ dataSource {
     pooled = true
 	driverClassName = "com.mysql.jdbc.Driver"
 	dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-    //driverClassName = "org.postgresql.Driver"
-    //dialect = "org.hibernate.dialect.PostgreSQLDialect"
     loggingSql=true
 }
 hibernate {
@@ -19,10 +17,7 @@ environments {
     development {
         dataSource {
             dbCreate = "none" // one of 'create', 'create-drop', 'update', 'validate', ''
-            //url = "jdbc:postgresql://localhost:5432/BeFit"
 			url = "jdbc:mysql://127.0.0.1:3306/befit?useUnicode=yes&characterEncoding=UTF-8"
-			//username="postgres"
-            //password="crnilu03"
 			username="befit"
 			password="L8Az7%UY"
         }
